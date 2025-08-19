5dedacee59c5c264ebfac2457caa5fdf6244e616
Miguel Cuellar <90780787+MacV99@users.noreply.github.com>
2025-08-19 18:13:14 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://macv99.github.io",
-  base: "/macia",
+  base: "/macia/",
   output: "static",
   trailingSlash: "always",
 });
