d75effbe9015935cca53562dcb63bec7f430c70a
Miguel Cuellar <90780787+MacV99@users.noreply.github.com>
2025-08-19 18:03:24 -0500

    deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// astro.config.mjs
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://macv99.github.io",
+  base: "/macia",
+  output: "static",
+});
