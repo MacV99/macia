29f3056a6f32daeea5385ab7769fc45b1e466cbe
Miguel Cuellar <90780787+MacV99@users.noreply.github.com>
2025-08-20 16:26:01 -0500

    bad fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
+// astro.config.mjs
 import { defineConfig } from "astro/config";
-
 export default defineConfig({
   site: "https://macv99.github.io",
   base: "/macia",
   output: "static",
+  trailingSlash: "always",
 });
